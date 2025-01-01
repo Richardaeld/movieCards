24b94e8bc61a5ae8978ca9fb450fb7c74002f56c
Richard E. <richardaeld@gmail.com>
2025-01-01 03:45:17 +0000

    added form input on Home

--- reactMovieTutorial/src/App.jsx
+++ reactMovieTutorial/src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react';
 import './App.css';
 import { MovieCard } from './components/MovieCard.jsx';
+import { Home } from './pages/Home.jsx';
 
 
 export function App() {
   return (
       <>
-        <MovieCard movie={{
-          title:"Film",
-          release_date: "2024"
-          }} />
+        <Home />
       </>
   )
 }
--- reactMovieTutorial/src/pages/Home.jsx
+++ reactMovieTutorial/src/pages/Home.jsx
@@ -1,14 +1,32 @@
 import { MovieCard } from "../components/MovieCard.jsx";
+import { useState } from "react"
 
 export function Home() {
+   const [searchQuery, setSearchQuery] = useState("");
+
    const movies = [
       {id: 1, title: 1, release_date: 1},
       {id: 2, title: 2, release_date: 2},
       {id: 3, title: 3, release_date: 3},
    ]
 
+   const handleSearch = e => {
+      e.preventDefault();
+      alert(searchQuery)
+   }
+
    return (
       <div className="home">
+         <form onSubmit={handleSearch} className="search-form">
+            <input 
+               type="text"
+               placeholder="Search for movies..."
+               className="search-input"
+               value={searchQuery}
+               onChange={e => setSearchQuery(e.target.value)}
+            />
+            <button type="submit" className="search-button">Search</button>
+         </form>
          <div className="movie-grid">
             {movies.map(movie => (
                <MovieCard movie={movie} key={movie.id} />
